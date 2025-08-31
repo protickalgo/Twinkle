258e71d037a7d7f29400a6de2568e122cbd2969a
Protick-Shikho <hasin.khan@shikho.com>
2025-09-01 00:16:01 +0600

    updated

--- main.go
+++ main.go
@@ -1,51 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "twinkle/domain"
-    "twinkle/handler"
-    "twinkle/repository"
-    "twinkle/service"
-    "github.com/gorilla/mux"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"fmt"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+	"twinkle/domain"
+	"twinkle/handler"
+	"twinkle/repository"
+	"twinkle/service"
 )
 
 func main() {
-    // Read environment variables
-    user := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASS")
-    host := os.Getenv("DB_HOST")
-    dbName := os.Getenv("DB_NAME")
-    port := os.Getenv("PORT")
+	// Read environment variables
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("PORT")
 
-    if port == "" {
-        port = "8080"
-    }
+	if port == "" {
+		port = "8080"
+	}
 
-    // Connect to PostgreSQL
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", host, user, password, dbName)
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Database connection failed:", err)
-    }
+	// Connect to PostgreSQL
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=require",
+		host, user, password, dbName,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
 
-    // Auto-migrate Product table
-    db.AutoMigrate(&domain.Product{})
+	// Auto-migrate Product table
+	db.AutoMigrate(&domain.Product{})
 
-    // Repository, Service, Handler
-    productRepo := repository.NewProductRepo(db)
-    productService := service.NewProductService(productRepo)
-    productHandler := handler.NewProductHandler(productService)
+	// Repository, Service, Handler
+	productRepo := repository.NewProductRepo(db)
+	productService := service.NewProductService(productRepo)
+	productHandler := handler.NewProductHandler(productService)
 
-    // Router and endpoints
-    r := mux.NewRouter()
-    r.HandleFunc("/products", productHandler.GetProducts).Methods("GET")
-    r.HandleFunc("/products", productHandler.CreateProduct).Methods("POST")
+	// Router and endpoints
+	r := mux.NewRouter()
+	r.HandleFunc("/products", productHandler.GetProducts).Methods("GET")
+	r.HandleFunc("/products", productHandler.CreateProduct).Methods("POST")
 
-    log.Println("Server running on port " + port)
-    http.ListenAndServe(":"+port, r)
+	log.Println("Server running on port " + port)
+	http.ListenAndServe(":"+port, r)
 }
